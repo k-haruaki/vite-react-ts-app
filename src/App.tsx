1e1c7095fb4cce5555131223c69c612f2ce0f25d
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-10-16 19:32:47 +0900

    NavLinkを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import { Routes, Route } from 'react-router-dom';
-import { About, Contact, Home } from './routes/index';
+import { Routes, Route, NavLink } from 'react-router-dom';
+import { About, Contact, Home, NoMatch } from './routes/index';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Hello React Router v6</h1>
+      <ul>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : undefined)}
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : undefined)}
+            to="/about"
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : undefined)}
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </li>
+      </ul>
       <Routes>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </div>
   );
