5652873f7b62e0a215c5f07af3caf86a1c8b70db
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-10-16 19:10:21 +0900

    ルーティング部分を設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import { About, Contact, Home } from './routes/index';
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <h1>Hello React Router v6</h1>
-      <Home />
-      <About />
-      <Contact />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
