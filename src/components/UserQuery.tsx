b3519f3a94337a799da6b3000075be90abaef521
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-10-17 20:51:37 +0900

    refetchOnWindowFocusのコメントなどを記載

--- src/components/UserQuery.tsx
+++ src/components/UserQuery.tsx
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query';
 import { User } from './User';
 
 const fetchUsers = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.co');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
   return res.json();
 };
 
@@ -10,11 +10,13 @@ const UserQuery = () => {
   const { data, isLoading, isError, error } = useQuery<User[], Error>(
     'users',
     fetchUsers,
+    {
+      // windowを表示した時にrefetchするかどうか（デフォルトはtrue）
+      // 例えば、タブなどを変更した場合などにrefetchされたりする
+      refetchOnWindowFocus: false,
+    },
   );
   console.log(data);
-  console.log(isLoading);
-  console.log(isError);
-  console.log(error);
 
   return (
     <div>
