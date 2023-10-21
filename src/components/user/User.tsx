2a05f06c08ccb221416f04b571068b34250267ff
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-10-21 19:04:41 +0900

    Omitのラッパーを実装

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -3,8 +3,13 @@ import userData from './data.json';
 
 export type User = typeof userData;
 
-// Omitを利用してUser型からageを除外
-export type UserWithoutAge = Omit<User, 'age'>;
+// Omitを利用してUser型からnameを除外
+export type UserWithoutAge = Omit<User, 'name'>;
+
+// 通常のOmitは第2引数に存在しない値を渡してもエラーにならない
+// タイポなどを防ぐため、Omitのラッパーを作成することがある。実装例
+export type MyOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+type myOmitUserWithoutAge = MyOmit<User, 'name'>;
 
 const fetchUsers = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
