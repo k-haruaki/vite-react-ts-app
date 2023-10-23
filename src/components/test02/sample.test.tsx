cc11528be1187b93325e6cf084ff2a75dc780bfc
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2023-10-23 23:53:14 +0900

    新しいテストコードのサンプルを実装

---
+++ src/components/test02/sample.test.tsx
@@ -0,0 +1,24 @@
+import { describe, it, expect } from 'vitest';
+import { primenumber } from './sample';
+
+describe('primenumber', () => {
+  it('should return false when n is less than 1', () => {
+    const result = primenumber(0);
+    expect(result).toBe(false);
+  });
+
+  it('should return false when n is not a prime number', () => {
+    const result = primenumber(4);
+    expect(result).toBe(false);
+  });
+
+  it('should return true when n is a prime number', () => {
+    const result = primenumber(7);
+    expect(result).toBe(true);
+  });
+
+  it('should return true when n is a large prime number', () => {
+    const result = primenumber(997);
+    expect(result).toBe(true);
+  });
+});
