2ec67048a2ac396d8d569cd0472f79c4e0e49a50
k-haruaki <65454615+k-haruaki@users.noreply.github.com>
2024-03-03 22:41:36 +0900

    test add number.

--- src/components/sampleTest/addNumbers.test.tsx
+++ src/components/sampleTest/addNumbers.test.tsx
@@ -5,4 +5,8 @@ describe('Calculate', () => {
   test('addNumbers', () => {
     expect(sample.addNumbers(3, 6)).toBe(9);
   });
+
+  test('addNumbers', () => {
+    expect(sample.addNumbers(6, 6)).toBe(12);
+  });
 });
